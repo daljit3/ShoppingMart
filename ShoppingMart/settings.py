9ef60a9385f23c538a113b70fa48c9299c9fe443
D Singh <daljit3@gmail.com>
2022-04-05 09:48:07 +0100

    Update settings.py

--- ShoppingMart/settings.py
+++ ShoppingMart/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q0pu6j7oyr2*)k_)kuk$i^px^*4wclca=h5l&qbyjs8aovoryv'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
